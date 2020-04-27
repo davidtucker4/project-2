e2ec1e92e6d50603c328731b4e446a4fcb5d3af7
David Tucker <dtucker4@uoregon.edu>
2020-04-27 15:07:58 -0700

    functions.js and README

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,53 @@
 // jshint esversion: 6
 
+//3-A
+let reverseWord = (word) => {
+  let result = "";
+
+  for (var i = 0; i < word.length; i++) {
+    result = word[i] + result;
+  }
+  return result;
+};
+
+let revWords1 = (sent) => {
+  let wordsArr = sent.split(" ");
+  let result = "";
+
+  for (var i = 0; i < wordsArr.length; i++) {
+    result = result + reverseWord(wordsArr[i]) + " ";
+  }
+  return result;
+};
+console.log(revWords1("Delta Echo"));
+
+//3-B
+let revWords2 = (sent) => {
+  let wordsArr = sent.split(" ");
+  let result = "";
+  wordsArr.forEach(
+    (word) => {
+      result = result + reverseWord(word) + " ";
+    }
+  );
+  return result;
+};
+
+console.log(revWords2("Delta Echo"));
+
+//3-C
+let revWords3 = (sent) => {
+  let wordsArr = sent.split(" ");
+  let result = "";
+  for (let word of wordsArr) {
+      result = result + reverseWord(word) + " ";
+    }
+  return result;
+};
+
+console.log(revWords3("Delta Echo"));
+
+//Extra Credit
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
